a1a8f249307bf4d01acf888e8332d8fb1434931b
Aryan Misra <aryanmisra4@gmail.com>
2019-04-19 14:23:56 -0400

    Finding the reverse complement for DNA strands

---
+++ revc.py
@@ -0,0 +1,6 @@
+seq = input("input: ")
+complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+bases = list(seq)
+bases = [complement[base] for base in bases]
+word = ''.join(bases)
+print(word[::-1])
